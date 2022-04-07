5d1cb61001aa0a46422e805362e8cdfa2785d25f
JAYLENGOMEZ <jaylengomez2792@gmail.com>
2022-04-07 15:53:00 -0400

    added some global variables working on functions

--- main.js
+++ main.js
@@ -2,5 +2,44 @@ let aiSequence = 0;
 let playSequence = 0;
 let aiArray = [];
 let playerArray = [];
+let blue = document.querySelector("#btn_one")
+let yellow = document.querySelector("#btn_two")
+let red = document.querySelector("#btn_three")
+let green = document.querySelector("#btn_four")
 
+const button = document.querySelectorAll('button')
 const colors = [blue, yellow, red, green]
+const startBtn = document.querySelector('.start')
+console.log(button)
+
+
+
+startBtn.addEventListener('click', () =>{
+    console.log('i worked')
+})
+// for (i = 0; i < colors.length; i++)
+// console.log(i)
+
+
+// button.addEventListener('click', () =>{
+//     for (let i = 0; i < colors.length; i++){
+//         console.log(colors[i])
+//     }
+// })
+
+blue.addEventListener('click', () =>{
+        console.log('yoyo')
+})
+yellow.addEventListener('click', () =>{
+        console.log('yoyo')
+})
+red.addEventListener('click', () =>{
+        console.log('yoyo')
+})
+green.addEventListener('click', () =>{
+        console.log('yoyo')
+})
+
+function userPattern(){
+    if('click')
+}
