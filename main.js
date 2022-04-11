0b29654272d657e8ba9a39d068eca6345abcb120
JAYLENGOMEZ <jaylengomez2792@gmail.com>
2022-04-11 16:18:26 -0400

    another new function updated button functionality

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let aiSequence = 0;
-let playSequence = 0;
+// let aiSequence = 0;
+// let playSequence = 0;
 let aiArray = [];
 let playerArray = [];
-let blue = document.querySelector("#btn_one")
-let yellow = document.querySelector("#btn_two")
-let red = document.querySelector("#btn_three")
-let green = document.querySelector("#btn_four")
+let blue = document.querySelector("#blue")
+let yellow = document.querySelector("#yellow")
+let red = document.querySelector("#red")
+let green = document.querySelector("#green")
 let aiTurn = false
 let playerTurn = false
-let round = 1
+let round = 0
 
 const button = document.querySelectorAll('button')
 const colors = [blue, yellow, red, green]
@@ -16,56 +16,80 @@ const startBtn = document.querySelector('.start')
 
 
 blue.addEventListener('click', () =>{
-        console.log('yoyo')
+        playerArray.push('blue')
+        blue.classList.add('changeColor');
+        setTimeout(() => {
+                blue.classList.remove('changeColor')
+        },500)
+        console.log(playerArray)
 })
 yellow.addEventListener('click', () =>{
-        console.log('yoyo')
+        playerArray.push('yellow')
+        yellow.classList.add('changeColor');
+        setTimeout(() => {
+                yellow.classList.remove('changeColor')
+        },500)
+        console.log(playerArray)
 })
 red.addEventListener('click', () =>{
-        console.log('yoyo')
+        playerArray.push('red')
+        red.classList.add('changeColor');
+        setTimeout(() => {
+                red.classList.remove('changeColor')
+        },500)
+        console.log(playerArray)
 })
 green.addEventListener('click', () =>{
-        console.log('yoyo')
+        playerArray.push('green')
+        green.classList.add('changeColor');
+        setTimeout(() => {
+                green.classList.remove('changeColor')
+        },500)
+        console.log(playerArray)
 })
 
+function newRound() {
+        round += 1;
+        console.log(`It is round ${round}`)
+      };
 
 function addRandomColor() {
     let addedColor = colors[parseInt(Math.random() * colors.length)]
-        // addedColor.classList.add('changeColor')
-        // console.log(addedColor)
         setTimeout(function() {
-                addedColor.classList.add('changeColor');
-                setTimeout(function() {
-                    addedColor.classList.remove("changeColor");
-                }, 500);
+        addedColor.classList.add('changeColor');
+        setTimeout(function() {
+        addedColor.classList.remove("changeColor");
+        }, 700);
         });
-}
+        aiArray.push(addedColor.id)
+        console.log(aiArray)
+        
+};
 
 
 
-function newRound() {
-    round += 1;
-    aiArray.push(addRandomColor())
-    console.log(aiArray)
-  }
+
 
 
   function startGame() {
-        // if (startBtn {
          if (aiTurn === false){
          aiTurn = true
-         playerTurn = true
          addRandomColor()
+         newRound()
  }
         
-  }
+  };
 
 
-
+  function playerRepeat() {
+        if (aiTurn === true)
+        aiTurn = false; 
+        playerTurn = true;
+  };
 
   startBtn.addEventListener('click', () =>
         startGame()
-  )
+  );
 
 
 
